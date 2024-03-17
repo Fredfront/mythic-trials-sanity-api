da73b8472a9e08e4f3f5612a958154301d9f5f81
fredrickvaagen <fredrick.vaagen@fjordline.com>
2024-03-17 20:24:10 +0100

    test image

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const fetch = require('node-fetch');
-require('dotenv').config();
+import createImageUrlBuilder from '@sanity/image-url';
+import express from 'express';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = 8080;
@@ -11,6 +14,16 @@ const projectId = process.env.SANITY_PROJECT_ID;
 const dataset = process.env.SANITY_DATASET;
 const token = process.env.SANITY_TOKEN;
 const discordWebhookUrl = process.env.DISCORD_WEBHOOK_URL;
+
+const imageBuilder = createImageUrlBuilder({
+  projectId: projectId || '',
+  dataset: dataset || '',
+});
+
+const urlForImage = (source) => {
+  if (!source) return '';
+  return imageBuilder.image(source).auto('format').fit('max').url() || ''; // Ensure to return an empty string if URL is not available
+};
 
 const baseUrl = `https://${projectId}.api.sanity.io/v1/data/mutate/${dataset}`;
 
@@ -30,13 +43,16 @@ app.post('/postToSanity', async (req, res) => {
     const data = await response.json();
 
     if (discordWebhookUrl && response.ok && data.results && (data.results[0].operation === 'update' || data.results[0].operation === 'create')) {
+      const teamName = mutations?.[0]?.createOrReplace?.teamName || 'N/A';
+      const imageUrl = urlForImage(mutations?.[0]?.createOrReplace?.image?.asset?._ref);
+
       await fetch(discordWebhookUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          content: `Nytt lag opprettet: ${mutations?.[0]?.createOrReplace?.teamName ?? 'Ukjent'}`,
+          content: `Nytt lag opprettet: ${teamName}, bilde: ${imageUrl}`,
         }),
       });
 
